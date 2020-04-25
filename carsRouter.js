1005a3030a308144ec46b2498d8b5d0dacd912f0
Candace Wilson <candaceyw@gmail.com>
2020-04-25 16:21:59 -0400

    CRUD complete and seed data created - STRETCH

--- carsRouter.js
+++ carsRouter.js
@@ -24,4 +24,36 @@ router.post('/', async (req, res, next) => {
 	}
 });
 
+// GET cars by ID
+router.get('/:id', async (req, res, next) => {
+	try {
+		const car = await db('cars').where('id', req.params.id).first();
+		res.json(car);
+	} catch (error) {
+		next(error);
+	}
+});
+
+// PUT update car
+router.put('/:id', async (req, res, next) => {
+	try {
+		const payload = req.body;
+		await db('cars').where('id', req.params.id).update(payload);
+		const updateCar = await db('cars').where('id', req.params.id).first();
+		res.json(updateCar);
+	} catch (error) {
+		next(error);
+	}
+});
+
+// DELETE car
+router.delete('/:id', async (req, res, next) => {
+	try {
+		await db('cars').where('id', req.params.id).del();
+		res.status(204).end();
+	} catch (error) {
+		next(error);
+	}
+});
+
 module.exports = router;
